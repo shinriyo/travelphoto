17ad6f62d5096dfa787140a27e26346a3d1ba914
shinriyo <shinriyo@gmail.comm>
2013-11-17 12:37:17 +0900

    Initial Commit

---
+++ travelphoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  travelphoto
+//
+//  Created by 臣輔 杉田 on 11/17/13.
+//  Copyright (c) 2013 shinriyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPAppDelegate class]));
+    }
+}
---
+++ travelphotoTests/travelphotoTests.m
@@ -0,0 +1,34 @@
+//
+//  travelphotoTests.m
+//  travelphotoTests
+//
+//  Created by 臣輔 杉田 on 11/17/13.
+//  Copyright (c) 2013 shinriyo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface travelphotoTests : XCTestCase
+
+@end
+
+@implementation travelphotoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
